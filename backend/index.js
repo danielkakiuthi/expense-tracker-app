3338729c4f35e9eeb958905b3d42560e365271c6
danielkakiuthi <daniel.kakiuthi@gmail.com>
2024-09-11 23:05:46 +0000

    added models and connection to mongodb database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import dotenv from "dotenv";
+
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { expressMiddleware } from "@apollo/server/express4";
@@ -9,7 +11,12 @@ import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHt
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
+
 const app = express();
+
 const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
@@ -30,5 +37,6 @@ app.use(
 );
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
 
 console.log(`🚀 Server ready at http://localhost:4000/`);
