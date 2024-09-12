4edb6001c419bb307903a7fb3637bbfc7fe76d6e
danielkakiuthi <daniel.kakiuthi@gmail.com>
2024-09-12 20:11:03 +0000

    transaction resolvers added

--- backend/typeDefs/transaction.typeDef.js
+++ backend/typeDefs/transaction.typeDef.js
@@ -13,13 +13,14 @@ const transactionTypeDef = `#graphql
   type Query {
     transactions: [Transaction!]
     transaction(transactionId: ID!): Transaction
+    # TODO => ADD categoryStatistics query
 
   }
 
   type Mutation {
     createTransaction(input: CreateTransactionInput!): Transaction!
     updateTransaction(input: UpdateTransactionInput!): Transaction!
-    deletetransaction(transactionId: ID!): Transaction!
+    deleteTransaction(transactionId: ID!): Transaction!
   }
 
   input CreateTransactionInput {
