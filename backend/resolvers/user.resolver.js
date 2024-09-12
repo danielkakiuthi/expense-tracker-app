51545d616de63384cbe1c6debf36897040c22b6b
danielkakiuthi <daniel.kakiuthi@gmail.com>
2024-09-12 18:11:01 +0000

    user resolvers added

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,15 +1,95 @@
 import { users } from "../dummyData/data.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 const userResolver = {
   Query: {
-    users: (_, __, { req, res }) => {
-      return users;
+    authUser: async (_, __, context) => {
+      try {
+        const user = await context.getUser();
+        return user;
+      } catch (err) {
+        console.log(`Error in authUser: ${err}`);
+        throw new Error("Internal server error");
+      }
     },
-    user: (_, { userId }) => {
-      return users.find((user) => user._id === userId);
+    user: async (_, { userId }) => {
+      try {
+        const user = await User.findById(userId);
+        return user;
+      } catch (err) {
+        console.log(`Error in user query: ${err}`);
+        throw new Error("Error getting user");
+      }
+    },
+    // TODO => ADD USER/TRANSACTION RELATION
+  },
+  Mutation: {
+    signUp: async (_, { input }, context) => {
+      try {
+        const { username, name, password, gender } = input;
+        if (!username || !name || !password || !gender) {
+          throw new Error("All fields are required");
+        }
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+          throw new Error("User already exists");
+        }
+
+        const salt = await bcrypt.gensalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const boyProfilePic = `http://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `http://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+          username,
+          name,
+          password: hashedPassword,
+          gender,
+          profilePicture: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+
+        await newUser.save();
+        await context.login(newUser);
+        return newUser;
+      } catch (err) {
+        console.log(`Error in signUp: ${err}`);
+        throw new Error(err.message || "Internal server error");
+      }
+    },
+
+    login: async (_, { input }, context) => {
+      try {
+        const { username, password } = input;
+        const { user } = await context.authenticate("graphql-local", {
+          username,
+          password,
+        });
+
+        await context.login(user);
+
+        return user;
+      } catch (err) {
+        console.log(`Error in login: ${err}`);
+        throw new Error(err.message || "Internal server error");
+      }
+    },
+
+    logout: async (_, __, {}) => {
+      try {
+        await context.logout();
+        req.session.destroy((err) => {
+          if (err) throw err;
+        });
+        res.clearCookie("connect.sid");
+        return { message: "Logged out successfully" };
+      } catch (err) {
+        console.log(`Error in logout: ${err}`);
+        throw new Error(err.message || "Internal server error");
+      }
     },
   },
-  Mutation: {},
 };
 
 export default userResolver;
