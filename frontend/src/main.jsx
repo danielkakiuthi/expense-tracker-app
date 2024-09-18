3df74723e3ef30eb9b529f03f5fe60b2c1ebe815
danielkakiuthi <daniel.kakiuthi@gmail.com>
2024-09-18 00:24:57 +0000

    changed `graphql` endpoint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,17 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import GridBackground from "./components/ui/GridBackground.jsx";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
